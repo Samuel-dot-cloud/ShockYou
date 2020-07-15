058572329fcadd30436247549d5dfa4f9590f968
Samuel-dot-cloud <successam336@gmail.com>
2020-07-15 20:17:00 +0300

    Creating an audio storer to store and access audio through shared preferences

---
+++ app/src/main/java/com/studiofive/shockyou/AudioModel.java
@@ -0,0 +1,63 @@
+package com.studiofive.shockyou;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class AudioModel implements Serializable {
+    int id;
+    String descriptionMessage;
+    String audioFileName;
+    boolean isAsset;
+    boolean isTTS;
+
+    // For asset audio only
+    public AudioModel(int id, String descriptionMessage, String audioFileName, boolean isAsset) {
+        this.id = id;
+        this.descriptionMessage = descriptionMessage;
+        this.audioFileName = audioFileName;
+        this.isAsset = isAsset;
+    }
+
+    // For TTS only
+    public AudioModel(int id, String descriptionMessage) {
+        this.id = id;
+        this.descriptionMessage = descriptionMessage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescriptionMessage() {
+        return descriptionMessage;
+    }
+
+    public String getAudioFileName() {
+        return audioFileName;
+    }
+
+    public boolean isAsset() {
+        return isAsset;
+    }
+
+    public boolean isTTS() {
+        return isTTS;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AudioModel that = (AudioModel) o;
+        return id == that.id &&
+                isAsset == that.isAsset &&
+                isTTS == that.isTTS &&
+                Objects.equals(audioFileName, that.audioFileName) &&
+                Objects.equals(descriptionMessage, that.descriptionMessage);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, audioFileName, descriptionMessage, isAsset, isTTS);
+    }
+}
---
+++ app/src/main/java/com/studiofive/shockyou/AudioStorer.java
@@ -0,0 +1,70 @@
+package com.studiofive.shockyou;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AudioStorer {
+    Context context;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
+    public AudioStorer(Context context) {
+        this.context = context;
+        preferences = context.getSharedPreferences(ShockUtils.SHOCK_SCARED_PREFS, Context.MODE_PRIVATE);
+        editor = preferences.edit();
+    }
+
+    public void storedAudios(List<AudioModel> audios) {
+        String key = context.getString(R.string.key_stored_audios);
+        Gson gson = new Gson();
+        editor.putString(key, gson.toJson(audios));
+        editor.commit();
+    }
+
+    private List<AudioModel> getStoredAudios(){
+        String audioAsString = preferences.getString(context.getString(R.string.key_stored_audios), null);
+        if (audioAsString == null || audioAsString.length() == 0){
+            return new ArrayList<>();
+        }
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<AudioModel>>(){}.getType();
+        return gson.fromJson(audioAsString, type);
+
+    }
+
+    public List<AudioModel> getAllAudios(){
+        List<AudioModel> audios = new ArrayList<>();
+        audios.add(new AudioModel(0, "Scream 2", "scream2", true));
+        audios.add(new AudioModel(1, "Behind You", "behind_you", true));
+        audios.add(new AudioModel(2, "Watching You", "watching_you", true));
+
+        audios.addAll(getStoredAudios());
+        return audios;
+    }
+
+    public AudioModel getSelectedAudio(){
+        List<AudioModel> audios = getAllAudios();
+
+        AudioModel defaultAudio = audios.get(0);
+
+        int audioId = preferences.getInt(context.getString(R.string.key_audio_id), 0);
+        for (AudioModel audio: audios){
+            if (audio.getId() == audioId){
+                return audio;
+            }
+        }
+        // Fall back on default
+        editor.putInt(context.getString(R.string.key_audio_id), 0);
+        editor.commit();
+        return defaultAudio;
+    }
+
+}
